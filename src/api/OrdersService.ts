0af08bc0c9a751cc3ffd697aaae488ae9837c47f
akulev <akulev@sweb,ru>
2022-02-02 10:38:35 +0300

    #10 added prettier

--- src/api/OrdersService.ts
+++ src/api/OrdersService.ts
@@ -1,7 +1,7 @@
 // import {instance} from "./tools";
-import {createApi} from "@reduxjs/toolkit/query/react";
-import {httpRequest, RTKResponse} from "./httpRequest";
-import {IOrders} from "../type/Types";
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { httpRequest, RTKResponse } from './httpRequest'
+import { IOrders } from '../type/Types'
 
 // export const ordersAPI = {
 //     getAllOrders: async () => {
@@ -22,10 +22,10 @@ export const OrdersService = createApi({
     baseQuery: httpRequest,
     endpoints: (build) => ({
         getAllOrders: build.query<IOrders, void>({
-            query: () => ({url: '/orders'}),
-            transformResponse: (response: RTKResponse<any>) => response.payload
-        })
-    })
+            query: () => ({ url: '/orders' }),
+            transformResponse: (response: RTKResponse<any>) => response.payload,
+        }),
+    }),
 })
 
-export const {useGetAllOrdersQuery} = OrdersService;
+export const { useGetAllOrdersQuery } = OrdersService
